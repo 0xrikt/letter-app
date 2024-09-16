135cf94e9168a59d2f676e5343824c18b71aa58b
0xrikt <taoyaqi1995@gmail.com>
2024-09-16 22:17:36 +0800

    Add Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import UserInfoForm from './components/UserInfoForm';
 import LetterGenerator from './components/LetterGenerator';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [userInfo, setUserInfo] = useState(null);
@@ -22,6 +23,7 @@ function App() {
           <LetterGenerator userInfo={userInfo} />
         )}
       </main>
+      <Analytics />
     </div>
   );
 }
